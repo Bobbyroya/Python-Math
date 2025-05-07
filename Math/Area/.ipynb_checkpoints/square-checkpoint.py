dcb213f4cca76614e64a2290eb1726df9f118c0e
Bobby <161744355+Bobbyroya@users.noreply.github.com>
2025-05-07 20:50:28 +0000

    I added more files and edited the readme

---
+++ Math/Area/.ipynb_checkpoints/square-checkpoint.py
@@ -0,0 +1,4 @@
+print('Calculate the area of a square')
+l = float(input("Enter the length: "))
+a = l**2
+print(f"The answer is {a}")
---
+++ Math/Area/rectangle.py
@@ -0,0 +1,5 @@
+print("Calculate the are of a rectangle")
+l = float(input("Enter the length: "))
+w = float(input("Enter the width: "))
+a = l*w
+print(f"The answer is {a}")
